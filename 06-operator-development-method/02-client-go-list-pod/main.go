146cf8208d1a866aa653d089cdd1b5c48e2209b8
Naka Masato <masatonaka1989@gmail.com>
2022-04-20 09:01:33 +0900

    fix

--- 06-operator-development-method/02-client-go-list-pod/main.go
+++ 06-operator-development-method/02-client-go-list-pod/main.go
@@ -20,19 +20,25 @@ func main() {
 	}
 
 	// Set kubeconfig flag
+	// String defines a string flag with specified name, default value, and usage string. The return value is the address of a string variable that stores the value of the flag.
+	// https://pkg.go.dev/flag#String
 	kubeconfig := flag.String("kubeconfig", defaultKubeConfigPath, "kubeconfig config file")
 	flag.Parse()
 
-	// Retrieve kubeconfig
+	// Get kubeconfig
+	// func BuildConfigFromFlags(masterUrl, kubeconfigPath string) (*restclient.Config, error)
+	// BuildConfigFromFlags is a helper function that builds configs from a master url or a kubeconfig filepath.
 	config, _ := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 
-	// Get clientset for kubernetes resources
+	// func NewForConfig(c *rest.Config) (*Clientset, error)
+	// NewForConfig creates a new Clientset for the given config.
+	// https://pkg.go.dev/k8s.io/client-go/kubernetes#NewForConfig
 	clientset, _ := kubernetes.NewForConfig(config)
 
-	// Get list of pod objects from all namespaces
+	// Get list of Pod objects from all namespaces
 	pods, _ := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
 
-	// show pod object to stdout
+	// Print Pod objects
 	fmt.Println("INDEX\tNAMESPACE\tNAME")
 	for i, pod := range pods.Items {
 		fmt.Printf("%d\t%s\t%s\n", i, pod.GetNamespace(), pod.GetName())
